a874e0457423ad27759bb69b59a239bb59326014
wj1203 <541569812@qq.com>
2019-11-21 12:19:21 +0800

    z字形字符串

---
+++ Offer/src/com/company/Convert.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Convert {
+
+    public static void main(String[] args) {
+        convert("abc",2);
+    }
+
+    /***/
+    public static String convert(String s, int numRows) {
+        if(numRows == 1)
+            return s;
+        StringBuilder strb = new StringBuilder();
+        for(int i=1;i<=numRows;i++){
+            if(i == 1 || i == numRows){
+                for(int j=i-1;j<s.length();j+=2*(numRows-1)){
+                    strb.append(s.charAt(j));
+                }
+            }
+            else{
+                int tag = 0;
+                int j=i-1;
+                while(j<s.length()){
+                    strb.append(s.charAt(j));
+                    if(tag % 2 == 0){
+                        tag++;
+                        j+=2*(numRows-i);
+                    }
+                    else{
+                        tag++;
+                        j+=2*(i-1);
+                    }
+                }
+            }
+        }
+        return strb.toString();
+    }
+}
